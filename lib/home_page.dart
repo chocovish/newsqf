a849f374d30accdd844a4bc66b9a64fef72532cb
chocovish <vishal.ghosh28@gmail.com>
2022-05-17 21:30:16 +0530

    added update delete

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -84,6 +84,42 @@ class _HomePageState extends State<HomePage> {
               children: [
                 ...data.map((e) => ListTile(
                       title: Text("${e["firstName"]} ${e["lastName"]}"),
+                      trailing: Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () async {
+                              await db.transaction((txn) async {
+                                await txn.rawUpdate(
+                                  "UPDATE users SET firstName = ?, lastName = ? WHERE id = ?",
+                                  [
+                                    fnController.text,
+                                    lnController.text,
+                                    e["id"]
+                                  ],
+                                );
+                              });
+                              await getData();
+                              fnController.clear();
+                              lnController.clear();
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () async {
+                              await db.transaction((txn) async {
+                                await txn.rawDelete(
+                                  "DELETE FROM users WHERE id = ?",
+                                  [e["id"]],
+                                );
+                              });
+                              await getData();
+                              fnController.clear();
+                              lnController.clear();
+                            },
+                          ),
+                        ],
+                      ),
                     )),
               ],
             )),
